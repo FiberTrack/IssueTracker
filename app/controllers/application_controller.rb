c2e035f04f389f396d44e528246d28af7f3932db
agraciaLpz <arnau.gracia.lopez@estudiantat.upc.edu>
2023-03-17 12:27:04 +0000

    Modificar text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "<h1>It works WASLAB04!</h1>".html_safe
+    render html: "<h1>It works FiberTracker!</h1>".html_safe
   end
 end
