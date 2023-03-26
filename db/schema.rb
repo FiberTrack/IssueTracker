2cfaa7112316eae60ee1c3e4e5de227144ab676a
gabrieldv01 <gabriel.del.valle@estudiantat.upc.edu>
2023-03-26 09:34:25 +0000

    acabat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_124553) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_26_000143) do
   create_table "issues", force: :cascade do |t|
     t.string "subject"
     t.text "description"
@@ -20,9 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_124553) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.string "provider"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
   end
 
 end
