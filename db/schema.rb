a2d2acdd5272a52a3ad6e50a9a00fb7c8ffb841e
agraciaLpz <arnau.gracia.lopez@estudiantat.upc.edu>
2023-04-05 17:39:21 +0000

    Afegit el tipus, severitat i prioritat als issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_26_000143) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_173615) do
   create_table "issues", force: :cascade do |t|
     t.string "subject"
     t.text "description"
     t.string "assign"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
+    t.string "severity"
+    t.string "priority"
   end
 
   create_table "users", force: :cascade do |t|
