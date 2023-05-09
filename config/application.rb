835403331206c2d7b9618e7ab999cbfe36b235cf
gabrieldv01 <gabriel.del.valle@estudiantat.upc.edu>
2023-05-09 09:20:24 +0000

    ignorar url de solicitud

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Waslab04
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_controller.forgery_protection_origin_check = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
