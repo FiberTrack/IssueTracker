ef759d30d11027f8d7d9654d0d6359ee49b97a4e
agraciaLpz <arnau.gracia.lopez@estudiantat.upc.edu>
2023-05-12 10:45:30 +0000

    Afegir delete

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,7 @@ module Waslab04
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
 
-    config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
+
 
     #
     # config.time_zone = "Central Time (US & Canada)"
