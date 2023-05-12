3a4a13919555c8d03d7a43a6745e558ad1bdc083
agraciaLpz <arnau.gracia.lopez@estudiantat.upc.edu>
2023-05-12 10:54:55 +0000

    Prova eliminar api key

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    skip_before_action :verify_authenticity_token, only: [:destroy]
   end
 end
