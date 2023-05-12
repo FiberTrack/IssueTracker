4af4049aa29fd1801b13c02b0fc321eeb7b1c4f1
agraciaLpz <arnau.gracia.lopez@estudiantat.upc.edu>
2023-05-12 10:57:52 +0000

    Prova eliminar api key3

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,6 @@
 class IssuesController < ApplicationController
   before_action :set_issue, only: %i[show edit update destroy]
+  skip_before_action :verify_authenticity_token, only: [:delete]
 
   def index
     @issues = Issue.all
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
-    skip_before_action :verify_authenticity_token, only: [:delete]
   end
 end
