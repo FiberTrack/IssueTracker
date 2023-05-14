e30b58d7240d9c4f6805f6b19ad0778e43be88df
gabrieldv01 <gabriel.del.valle@estudiantat.upc.edu>
2023-05-14 17:00:39 +0000

    log

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -76,6 +76,7 @@ end
     puts request.headers['Authorization']
     user_created = User.find_by(api_key: request.headers['Authorization'])
 
+    Rails.logger.info 'Esta es mi id: #{user_created.id}'
 
     respond_to do |format|
       if @issue.save
