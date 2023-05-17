f807df30c59240be8db752e97607950db354cef8
gabrieldv01 <gabriel.del.valle@estudiantat.upc.edu>
2023-05-17 14:36:00 +0000

    fet

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -179,7 +179,14 @@ end
         return false
       end
     end
-
+    watcher_ids = issue_params[:watcher_ids]
+    watcher_ids.each do |uid|
+      user = User.find_by(uid: uid)
+      if user.nil? and !uid.empty?
+        render json: { error: 'Invalid value watcher_ids.' }, status: :bad_request
+        return false
+      end
+    end
     return true
   end
 
