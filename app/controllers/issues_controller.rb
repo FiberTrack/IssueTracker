74f3ed914d7390b3bd3f673cef13293aba476adb
gabrieldv01 <gabriel.del.valle@estudiantat.upc.edu>
2023-05-17 13:51:31 +0000

    .

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -147,7 +147,7 @@ end
     end
     status_issue = issue_params[:status]
     if status_issue.nil? || status_issue.empty?
-      issue_params[:status] = "New"
+      issue_params[:status] = 'New'
     end
     if status_issue.present?
       if status_issue.blank? || !%w[New In\ Progress Ready\ For\ Test Postponed Closed Information\ Needed Rejected].include?(status_issue)
