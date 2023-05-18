b1bc28aaa6a6b2bfc8ebf6c776a2bfc22e5fbec5
Abdelrahim Chelh El Azzaoui <abdelrahim.chelh@estudiantat.upc.edu>
2023-05-18 17:53:03 +0000

    sense parametre

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,30 +23,55 @@ def update_avatar(avatar)
   require 'aws-sdk-s3'
 
      # Obtener la URL de la imagen anterior y eliminar el objeto S3 asociado
+  if current_user
+    old_avatar_url = current_user.avatar_url
+    if old_avatar_url.present?
+      old_object_key = URI.parse(old_avatar_url).path[1..-1]
+      s3 = Aws::S3::Resource.new
+      bucket = s3.bucket("fibertracker-bucket")
+      object = bucket.object(old_object_key)
+      object.delete
+    end
 
-  old_avatar_url = current_user.avatar_url
-  if old_avatar_url.present?
-    old_object_key = URI.parse(old_avatar_url).path[1..-1]
+    # Obtener el archivo subido por el usuario
+    file = avatar
+
+    # Generar un nombre único para el archivo
+    filename = SecureRandom.hex(10) + File.extname(file.original_filename)
+
+    # Subir el archivo al bucket de S3
     s3 = Aws::S3::Resource.new
     bucket = s3.bucket("fibertracker-bucket")
-    object = bucket.object(old_object_key)
-    object.delete
+    object = bucket.object(filename)
+    object.upload_file(file.tempfile)
+
+    # Actualizar la URL de la imagen en la base de datos del usuario
+    current_user.update_attribute(:avatar_url, object.public_url)
+  else
+    old_avatar_url = @authenticated_user.avatar_url
+    if old_avatar_url.present?
+      old_object_key = URI.parse(old_avatar_url).path[1..-1]
+      s3 = Aws::S3::Resource.new
+      bucket = s3.bucket("fibertracker-bucket")
+      object = bucket.object(old_object_key)
+      object.delete
+    end
+
+    # Obtener el archivo subido por el usuario
+    file = avatar
+
+    # Generar un nombre único para el archivo
+    filename = SecureRandom.hex(10) + File.extname(file.original_filename)
+
+    # Subir el archivo al bucket de S3
+    s3 = Aws::S3::Resource.new
+    bucket = s3.bucket("fibertracker-bucket")
+    object = bucket.object(filename)
+    object.upload_file(file.tempfile)
+
+    # Actualizar la URL de la imagen en la base de datos del usuario
+    @authenticated_user.update_attribute(:avatar_url, object.public_url)
   end
-
-  # Obtener el archivo subido por el usuario
-  file = avatar
-
-  # Generar un nombre único para el archivo
-  filename = SecureRandom.hex(10) + File.extname(file.original_filename)
-
-  # Subir el archivo al bucket de S3
-  s3 = Aws::S3::Resource.new
-  bucket = s3.bucket("fibertracker-bucket")
-  object = bucket.object(filename)
-  object.upload_file(file.tempfile)
-
-  # Actualizar la URL de la imagen en la base de datos del usuario
-  current_user.update_attribute(:avatar_url, object.public_url)
 
 end
 
