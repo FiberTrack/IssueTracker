974f807843227c231a17f064c5c3c72586ba0a83
gabrieldv01 <gabriel.del.valle@estudiantat.upc.edu>
2023-05-18 10:37:40 +0000

    prova salt de linia

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -33,7 +33,12 @@ end
     elsif params[:options].present?
       opcions = params[:options]
       if !opcions.all? { |id| !id.blank? && %w[Wishlist Minor Normal Important Critical Bug Question Enhancement Low Normal High New In\ Progress Ready\ For\ Test Postponed Closed Information\ Needed Rejected ].include?(id) || User.exists?(full_name: id) }
-        response = {error: "Each option parameter must be one of the following:"+"\\"+"For Severity: Wishlist Minor Normal Important Critical"+"\nFor Issue_type: Bug Question Enhancement"+"\nFor Priority: Low Normal High"+"\nFor Status: New In\ Progress Ready\ For\ Test Postponed Closed Information\ Needed Rejected"+"\nFor Assign: The full_name of one of the logged in users"}
+        response = {error: "Each option parameter must be one of the following:\n" \
+         "For Severity: Wishlist Minor Normal Important Critical\n" \
+         "For Issue_type: Bug Question Enhancement\n" \
+         "For Priority: Low Normal High\n" \
+         "For Status: New In Progress Ready For Test Postponed Closed Information Needed Rejected\n" \
+         "For Assign: The full_name of one of the logged in users"}
         render json: response.as_json , status: :bad_request
       end
       options = params[:options].map(&:downcase)
